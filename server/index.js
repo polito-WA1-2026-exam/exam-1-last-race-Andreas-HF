e9adc050dd2939bc641b750fbc9e458f398dd0ef
Andreas-HF <github@mail.hflo.no>
2026-06-11 18:19:41 +0200

    feat: base dependencies setup and /health endpoint

--- server/index.js
+++ server/index.js
@@ -1,11 +1,32 @@
 // imports
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+
+import passport from "passport";
+import LocalStrategy from "passport-local";
+import session from "express-session";
 
 // init express
 const app = new express();
 const port = 3001;
 
+// middleware
+app.use(express.json());
+app.use(morgan("dev"));
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  optionsSuccessStatus: 200,
+  credentials: true
+};
+app.use(cors(corsOptions));
+
 // activate the server
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
+});
+
+app.get("/api/health", (req, res) => {
+  return res.status(200).send("UP")
 });
